bc2c705970a7dbc91d524dcdf1e96a8097c48ceb
[estebans0] <[estebanjs029@gmail.com]>
2022-04-03 13:20:21 -0600

    Estructura del trabajo

--- tp1.py
+++ tp1.py
@@ -1,2 +1,10 @@
 #Elaborado por:
-#Fecha: 03/04/2022
+# Fecha de Creación: 03/04/2022 01:20pm
+# Fecha de última Modificación: XX/04/2022 XX:XXpm
+# Versión: 3.9.2
+
+# Definición de Funciones
+
+# Definición de Validaciones
+
+# Programa Principal
